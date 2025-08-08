275103c7b1ca6f27268867c88094b0a9078f39be
fernandamatoscamargo-cmd <fernanda.matos.camargo@escola.pr.gov.br>
2025-08-08 12:08:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Filme',
+    'Qual filme mais conhecido da disney?',  
+    'Cinderela'  
+)
+
+criaCartao(
+    'Animal',  
+    'Qual animal é conhecido como “melhor amigo do homem”?', 
+    'Cachorro' 
+)
+
+criaCartao(
+    'Cidades',   
+    'Qual cidade é famosa pelos seus cassinos?',  
+    'Las Vegas' 
+)
+
+criaCartao(
+    'Música',
+    'Grupo de Rap brasileiro mais conhecido?', 
+    'Racionais Mc’s'
+)
+
